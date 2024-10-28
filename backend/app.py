9afb9742d61cf0aa15448a956c5b1d6a073ac84e
samreen <samreenfatima02912@gmail.com>
2024-10-28 09:19:25 +0530

    integrated code

--- backend/app.py
+++ backend/app.py
@@ -178,7 +178,12 @@ from langchain_community.llms import Ollama
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
+from transformers import pipeline
 # Initialize the app
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Change this to a secure random key
@@ -207,6 +212,7 @@ class Prescription(db.Model):
 with app.app_context():
     db.create_all()
 
+folder_path="db"
 # Use a CPU-friendly embedding model
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
@@ -264,9 +270,9 @@ def generate_prescription():
     db.session.commit()
 
     return jsonify({"message": "Prescription generated successfully.", "data": prescription_content}), 200
-
+'''
 # Endpoint for testing the model
-@app.route("/ai", methods=["POST"])
+@app.route("/ask_pdf", methods=["POST"])
 def aiPost():
     json_content = request.json
     query = json_content.get("query")
@@ -277,7 +283,7 @@ def aiPost():
 # Uploading files to the PDF directory
 @app.route("/pdf", methods=["POST"])
 def pdfPost():
-    file = request.files["pdf"]
+    file = request.files["file"]
     file_name = file.filename
     save_file = "pdf/" + file_name
     file.save(save_file)
@@ -289,12 +295,116 @@ def pdfPost():
         "chunks": 0,
     }
     return response
+'''
+
+# sending the prompt to the model input is the input and context is from the db
+raw_prompt = PromptTemplate.from_template(
+    """ 
+    <s>[INST] You are a technical assistant good at searching docuemnts. If you do not have an answer from the provided information say so. [/INST] </s>
+    [INST] {input}
+           Context: {context} 
+           Answer:
+    [/INST]
+"""
+)
+
+# the api end point for taking the query input 
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("Post /ai called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    # response = cached_llm.invoke(query)
+    response="This endpoint is working fine"
+
+    print(response)
+
+    response_answer = {"answer": response}
+    return response_answer
+
+# asking questions from these pdfs
+
+@app.route("/ask_pdf", methods=["POST"])
+def askPDFPost():
+    print("Post /ask_pdf called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    print("Loading vector store")
+    vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
+    # creating retreiver
+    print("Creating chain")
+    retriever = vector_store.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={
+            "k": 20, # picking 20 most similar chunks
+            "score_threshold": 0.1,
+        },
+    )
+
+    document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
+    chain = create_retrieval_chain(retriever, document_chain)
+
+    result = chain.invoke({"input": query})
+
+    print(result)
+
+    sources = []
+    for doc in result["context"]:
+        sources.append(
+            {"source": doc.metadata["source"], "page_content": doc.page_content}
+        )
+
+    response_answer = {"answer": result["answer"], "sources": sources}
+    return response_answer
+
+# uploading files to the pdf directory 
+@app.route("/pdf", methods=["POST"])
+def pdfPost():
+    file = request.files["file"]
+    file_name = file.filename
+    save_file = "pdf/" + file_name
+    file.save(save_file)
+    print(f"filename: {file_name}")
+    # check if its saved in the pdf directory
+    '''
+    response ={"status":"successfully uploaded","file_name":file_name}
+    print(response)
+    return response
+    '''
+   
+    loader = PDFPlumberLoader(save_file)
+    docs = loader.load_and_split()
+    print(f"docs len={len(docs)}")
+
+    chunks = text_splitter.split_documents(docs)
+    print(f"chunks len={len(chunks)}")
+
+    vector_store = Chroma.from_documents(
+        documents=chunks, embedding=embedding, persist_directory=folder_path
+    )
+
+    vector_store.persist()
+
+    response = {
+        "status": "Successfully Uploaded",
+        "filename": file_name,
+        "doc_len": len(docs),
+        "chunks": len(chunks),
+    }
+    return response
 
 def start_app():
     app.run(host="0.0.0.0", port=8080, debug=True)
 
 if __name__ == "__main__":
     start_app()
+
 
 
 # based on patient names retreival (trial)
